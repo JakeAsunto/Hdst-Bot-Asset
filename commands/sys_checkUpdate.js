da93453ff67271eed96207b383b4df7e6eebc6a7
Hadestia <rjchristianroldan@gmail.com>
2023-04-02 12:42:16 +0800

    updated file(s)

--- commands/sys_checkUpdate.js
+++ commands/sys_checkUpdate.js
@@ -1,6 +1,6 @@
 module.exports.config = {
 	name: 'check-update',
-	version: '1.0.6',
+	version: '2.0.6',
 	hasPermssion: 2,
 	commandCategory: 'system',
 	aliases: [ 'update', 'bot-update' ],
@@ -10,27 +10,27 @@ module.exports.config = {
 	credits: 'Hadestia'
 }
 
-module.exports.run = async function ({ api, args, event, textFormat }) {
+module.exports.run = async function ({ api, args, event, Utils }) {
 	
 	const axios = require('axios');
 	
-	const assets = require(`${__dirname}/../../json/!asset-update.json`);
-	await axios(`${global.config.REPO}json/!asset-update.json`).then(res => {
+	const assets = require(`${global.HADESTIA_BOT_CLIENT.mainPath}/json/!asset-update.json`);
+	await axios(`${global.HADESTIA_BOT_CONFIG.REPO}json/!asset-update.json`).then(res => {
 		if (res.data.VERSION !== global.BOT_VERSION) {
 			return api.sendMessage(
-				textFormat('system', 'botUpdatesFound', res.data.VERSION, res.data.CHANGELOGS),
+				Utils.textFormat('system', 'botUpdatesFound', res.data.VERSION, res.data.CHANGELOGS),
 				event.threadID,
 				event.messageID
 			);
 		}
 		return api.sendMessage(
-			`${textFormat('system', 'botUpdatesUpToDate')} current version was ${global.BOT_VERSION}`,
+			`${Utils.textFormat('system', 'botUpdatesUpToDate')} current version was ${global.BOT_VERSION}`,
 			event.threadID,
 			event.messageID
 		);
 	}).catch(e => {
 		return api.sendMessage(
-			textFormat('system', 'botVersionFetchingFailed'),
+			Utils.textFormat('system', 'botVersionFetchingFailed'),
 			event.threadID,
 			event.messageID
 		);
